1b16a54480853932301450087db479e725003990
JoBang-Ch <douyuob479839@gmail.com>
2026-04-08 10:46:56 +0000

    week 1 exercise

--- week1/index.js
+++ week1/index.js
@@ -1,1 +1,14 @@
-const express = require("express");
+const express = require('express');
+const app = express();
+
+app.get('/hello', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.get('/bye', (req, res) => {
+  res.send('Goodbye, World!');
+});
+
+app.listen(8888, () => {
+  console.log('Server running on port 8888');
+});
