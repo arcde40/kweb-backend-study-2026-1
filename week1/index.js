f9cf520d3273305d34df517f8a55dbd3cafe33d9
msterk11 <minsukang1124@gmail.com>
2026-04-12 04:16:08 +0000

    week 1 exercise

--- week1/index.js
+++ week1/index.js
@@ -1,1 +1,13 @@
 const express = require("express");
+const app = express();
+app.get("/hello", (req, res) => {
+    res.send("Hello, World!");
+});
+
+app.get("/bye", (req, res) => {
+    res.send("Goodbye, World!");
+});
+
+app.listen(8080, () => {
+    console.log('Server listening on port 8080!');
+});
