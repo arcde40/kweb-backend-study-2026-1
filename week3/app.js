152958da7523a4ad32126d036d6f762d4d3a760f
arcde40 <arcde40@gmail.com>
2026-05-06 06:24:45 +0000

    feat: week3 complete

--- week3/app.js
+++ week3/app.js
@@ -11,4 +11,36 @@ const tasks = [
 	{name: "Default", completed: false},
 ];
 
-/* Your Code Here */
+app.get('/todo', (req, res) => {
+	res.render('todo-improved.pug', { tasks, error: req.query.error });
+});
+
+app.post('/add-task', (req, res, next) => {
+	const name = req.body.taskName;
+	if(name && name.trim() !== '') {
+		tasks.push({name, completed: false});
+		next();
+	} else {
+		next("할 일이 비어있습니다!");
+	}
+});
+
+app.post('/complete-task', (req, res, next) => {
+	const index = Number(req.body.taskIndex);
+	if(index < 0 || index >= tasks.length) {
+		next("없는 일 번호입니다!");
+		return;
+	}
+	tasks[index].completed = true;
+	next();
+});
+
+app.use((req, res, next) => {
+	res.redirect('/todo');
+}) 
+
+app.use((error, req, res, next) => {
+	res.redirect(`/todo?error=${error}`);
+});
+
+app.listen(8080, ()=>{});
