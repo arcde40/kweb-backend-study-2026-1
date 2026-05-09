4bafac6d3d1ee42bb95cb0a657ca9a98d1d340a9
shyeonu-wq <shyeonu@korea.ac.kr>
2026-05-09 13:48:24 +0000

    Complete calculator assignment

--- week3/app.js
+++ week3/app.js
@@ -2,13 +2,47 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.set('views', `${__dirname}/views`);
 app.set('view engine', 'pug');
 app.use(express.static('public'));
 
-const tasks = [
-	{name: "Default", completed: false},
-];
+app.get('/', (req, res) => {
+  res.redirect('/calculator');
+});
 
-/* Your Code Here */
+app.get('/calculator', (req, res) => {
+  res.render('calculator', {
+	result: null,
+	error: null
+  });
+});
+
+app.post('/calculate', (req, res) => {
+  const num1 = Number(req.body.num1);
+  const num2 = Number(req.body.num2);
+
+  if (isNaN(num1) || isNaN(num2)) {
+	return res.render('calculator', {
+	  result: null,
+	  error: '두 수를 모두 입력해주세요.'
+	});
+  }
+
+  const sum = num1 + num2;
+  const diff = num1 - num2;
+
+  res.render('calculator', {
+	result: {
+	  num1,
+	  num2,
+	  sum,
+	  diff
+	},
+	error: null
+  });
+});
+
+app.listen(8080, () => {
+  console.log('Server is running on port 8080');
+});
