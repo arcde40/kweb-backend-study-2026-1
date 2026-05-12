9174246ee4b984d3bd599033d0a30af2d1d3fc57
moolgae1 <moolgae6838@gmail.com>
2026-05-12 15:34:58 +0000

    week2 assignment done

--- week2/app.js
+++ week2/app.js
@@ -21,11 +21,9 @@ app.get("/user/:id", (req, res) => {
 const foodRouter = require('./food');
 app.use('/food', foodRouter);
 
-app.get("/math/sum", (req, res) => {
-    const a = req.query.a;
-    const b = req.query.b;
-    res.send(`${a} + ${b} 는 ${Number(a)+Number(b)} 입니다!`);
-});
+const mathRouter = require('./math');
+app.use('/math', mathRouter);
+
 
 
 app.listen(8080, () => {
