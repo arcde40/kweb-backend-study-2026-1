cc7e489bae5c575b6f33ea9fea069a0167a5c6a8
arcde40 <arcde40@gmail.com>
2026-06-03 06:23:00 +0000

    week5 complete

--- week5/app.js
+++ week5/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const session = require('express-session');
 
+let sessionId = 1;
+
 const app = express();
 
 app.use(session({
@@ -11,18 +13,35 @@ app.use(session({
 
 app.get('/login', (req, res) => {
 	const name = req.query.name;
+
+	req.session.user = {
+		id: sessionId++,
+		name: name
+	}
+	
+	return res.send(`Hi, ${name}! Your id is: ${id}`);
 });
 
 app.get('/logout', (req, res) => {
-
+	req.session.destroy(err => {
+		if(err) return res.send('Something went wrong...');
+		return res.send('Success!');
+	});
 });
 
 app.get('/tell', (req, res) => {
 	const msg = req.query.msg;
+	const {user} = req.session;
+	if(!user) return res.send('You are not logged in!');
+	user.msg = msg;
+	return res.send("I remember you!");
 });
 
 app.get('/ask', (req, res) => {
-
+	const {user} = req.session;
+	if(!user) return res.send('You are not logged in!');
+	const {id, name, msg} = user;
+	return res.send(`id: ${id}, name: ${name}, your message: ${msg}`);
 });
 
 app.listen(8080, async ()=>{
