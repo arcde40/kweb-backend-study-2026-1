e1926570d762c0847ae17c28794ef4e736f19f93
jmk133 <70556269+jmk133@users.noreply.github.com>
2026-06-03 14:39:07 +0000

    week5 assignment

--- week5/app.js
+++ week5/app.js
@@ -3,6 +3,8 @@ const session = require('express-session');
 
 const app = express();
 
+let sessionid = 1;
+
 app.use(session({
 	secret: 'asdf',
 	resave: false,
@@ -10,19 +12,42 @@ app.use(session({
 }));
 
 app.get('/login', (req, res) => {
+	const {user} = req.session;
+	if (user) return res.send('Your are already logged in!');
+
 	const name = req.query.name;
+	const id = sessionid++;``
+
+	req.session.user = {
+		id: id,
+		name: name,
+	};
+
+	return res.send(`Hi, ${name}, Your session id = ${id}.`)
 });
 
 app.get('/logout', (req, res) => {
-
+	req.session.destroy(err => {
+		if (err) return req.send('Something went wrong...');
+		return res.send('Sucess!');
+	})
 });
 
 app.get('/tell', (req, res) => {
 	const msg = req.query.msg;
+	const user = req.session;
+
+	if (!user) return res.send('Your are not logged in!');
+	user.msg = msg;
+	return res.send('I remember you!');
 });
 
 app.get('/ask', (req, res) => {
+	const {user} = req.session;
+	if (!user) return res.send('Your are not logged in!');
+	const {id, name, msg} = user;
 
+	return res.send(`Your id = ${id}, name = ${name}, msg = ${msg}`)
 });
 
 app.listen(8080, async ()=>{
