77b0706957f9eea9d3eb5ad8ac121583c6d4a7d7
egn27-wq <egn27@korea.ac.kr>
2026-06-23 15:20:13 +0000

    과제5

--- week5/app.js
+++ week5/app.js
@@ -3,6 +3,7 @@ const session = require('express-session');
 
 const app = express();
 
+let sessionID = 1;
 app.use(session({
 	secret: 'asdf',
 	resave: false,
@@ -10,19 +11,38 @@ app.use(session({
 }));
 
 app.get('/login', (req, res) => {
+	const { user } = req.session;
+	if(user) return res.send(`you are already logged in!`);
 	const name = req.query.name;
+	const Id = sessionID++;
+	req.session.user ={
+		Id,
+		name
+	}
+	return res.send(`hi ${name} your session id is ${Id}`)
 });
 
 app.get('/logout', (req, res) => {
-
+	req.session.destory (err=>{
+		if(err) return req.send('something wrong');
+		res.send('success');
+	})
 });
 
 app.get('/tell', (req, res) => {
 	const msg = req.query.msg;
+	const { user } = req.session;
+
+	if(!user) return res.send(`you are not logged in!`);
+	user.msg = msg;
+	return res.send(`i rememeber you!`)
 });
 
 app.get('/ask', (req, res) => {
-
+	const { user } = req.session;
+	if(!user) return res.send(`you are not logged in!`);
+	const {Id, name, msg} = user;
+	return res.send(`hi ${name} your session id is ${Id} msg: ${user.msg}`);
 });
 
 app.listen(8080, async ()=>{
